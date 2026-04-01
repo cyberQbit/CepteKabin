2582ed27b7048ef78ba9d96301a01903cbfca8de
Aydın AYDEMİR <aydinaydemir2006@hotmail.com>
2026-04-01 16:50:13 +0300

    feat: Implement formal FileProvider sharing system for combinations (Kombin) with deep linking and cache support

--- app/src/main/java/com/cyberqbit/ceptekabin/util/KombinShareHelper.kt
+++ app/src/main/java/com/cyberqbit/ceptekabin/util/KombinShareHelper.kt
@@ -1,6 +1,8 @@
 package com.cyberqbit.ceptekabin.util
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.core.content.FileProvider
@@ -168,3 +170,4 @@ object KombinShareHelper {
         }
     }
 }
+
